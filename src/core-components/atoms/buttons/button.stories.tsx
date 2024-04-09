e47cec2567eb8af7942f3bea7b05894d450ed9d8
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-04-09 13:04:11 +0530

    old storybook

--- src/core-components/atoms/buttons/button.stories.tsx
+++ src/core-components/atoms/buttons/button.stories.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 import {Button} from './buttons.component';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Atoms/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Disable: ComponentStory<typeof Button> = () => (
   <Button className="bg-gray-light w-40" onClick={() => {}}>
     Next
