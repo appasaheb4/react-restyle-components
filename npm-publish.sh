3c4a895b85be6cd262fcb43828c01298aa298e06
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-06-11 18:34:42 +0530

    new format

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,14 +1,17 @@
-cp package2.json package.json
-yarn version --patch
+# cp package2.json package.json
+# yarn version --patch
 
-# cjs
-copyfiles -u 1 src/library/assets/svg/*.svg lib/cjs
-copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/cjs
-copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/cjs
+# # cjs
+# copyfiles -u 1 src/library/assets/svg/*.svg lib/cjs
+# copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/cjs
+# copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/cjs
 
-# esm
-copyfiles -u 1 src/library/assets/svg/*.svg lib/esm
-copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/esm
-copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/esm
+# # esm
+# copyfiles -u 1 src/library/assets/svg/*.svg lib/esm
+# copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/esm
+# copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/esm
 
-cp package.json lib/package.json
+# cp package.json lib/package.json
+
+copyfiles -u 1 src/library/assets/svg/*.svg lib/src
+copyfiles -u 1 src/**/*.css lib/src
