40a027325130cced762b0256ab0cba34daa41b80
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-06-11 15:17:52 +0530

    css file move

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,2 +1,14 @@
 cp package2.json package.json
-# yarn version --patch
+yarn version --patch
+
+# cjs
+copyfiles -u 1 src/library/assets/**/* lib/cjs
+copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/cjs
+copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/cjs
+
+# esm
+copyfiles -u 1 src/library/assets/**/* lib/esm
+copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/esm
+copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/esm
+
+cp package.json lib/package.json
