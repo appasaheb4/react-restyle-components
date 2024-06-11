64052d20a7ff9091c9b423680349f5eea627226b
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-06-11 15:25:18 +0530

    only svg file upload

--- npm-publish.sh
+++ npm-publish.sh
@@ -2,12 +2,12 @@ cp package2.json package.json
 yarn version --patch
 
 # cjs
-copyfiles -u 1 src/library/assets/**/* lib/cjs
+copyfiles -u 1 src/library/assets/svg/*.svg lib/cjs
 copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/cjs
 copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/cjs
 
 # esm
-copyfiles -u 1 src/library/assets/**/* lib/esm
+copyfiles -u 1 src/library/assets/svg/*.svg lib/esm
 copyfiles -u 1 src/core-components/atoms/input/input-otp.styles.css  lib/esm
 copyfiles -u 1 src/core-components/atoms/input/input.styles.css  lib/esm
 
