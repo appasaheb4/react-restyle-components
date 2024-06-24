0025d410ab36cbbd7be8d85f1dd5b071caead221
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-06-24 19:12:51 +0530

    src folder remove

--- src/core-components/atoms/loader/loader.test.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable testing-library/render-result-naming-convention */
-import React from 'react';
-import {render} from '@core-utils';
-import {Loader} from './loader.component';
-
-it('render Loader correctly', () => {
-  const loader = render(<Loader />);
-  expect(loader).toMatchSnapshot();
-});
