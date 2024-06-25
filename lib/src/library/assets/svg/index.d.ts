c7c1ce4a5d4f71a1770d9ac89812637887375ee7
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-06-25 13:47:20 +0530

    lib create

--- lib/src/library/assets/svg/index.d.ts
+++ lib/src/library/assets/svg/index.d.ts
@@ -1,9 +1,9 @@
-import UnCheckbox from '../../../library/assets/svg/unCheckbox.svg';
-import CheckedBox from '../../../library/assets/svg/checkedBox.svg';
-import DatePickerSvg from '../../../library/assets/svg/datePicker.svg';
-import CheckedRadio from '../../../library/assets/svg/checkedRadio.svg';
-import UncheckRadio from '../../../library/assets/svg/uncheckRadio.svg';
-import TimerSVG from '../../../library/assets/svg/timer.svg';
-import UpArrow from '../../../library/assets/svg/UpArrow.svg';
-import DownArrow from '../../../library/assets/svg/DownArrow.svg';
+import UnCheckbox from './unCheckbox.svg';
+import CheckedBox from './checkedBox.svg';
+import DatePickerSvg from './datePicker.svg';
+import CheckedRadio from './checkedRadio.svg';
+import UncheckRadio from './uncheckRadio.svg';
+import TimerSVG from './timer.svg';
+import UpArrow from './UpArrow.svg';
+import DownArrow from './DownArrow.svg';
 export { UnCheckbox, CheckedBox, DatePickerSvg, CheckedRadio, UncheckRadio, TimerSVG, UpArrow, DownArrow, };
