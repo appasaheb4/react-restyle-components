f7add8189e064e6d6bebe188f8ed4c5a46b28ca3
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-06-28 11:15:33 +0530

    new comp added multi select with fileds

--- lib/src/core-components/molecules/mutiple-select/multi-select.component.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface MultipleSelectProps {
-    options: Array<string>;
-    selectedItems?: Array<string>;
-    hasError?: boolean;
-    onSelect: (item: any) => any;
-}
-export declare const MultipleSelect: ({ options, selectedItems, hasError, onSelect, }: MultipleSelectProps) => React.JSX.Element;
-export {};
--- lib/src/core-components/molecules/mutiple-select/multi-select.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { MultipleSelect } from './multi-select.component';
-declare const meta: Meta<typeof MultipleSelect>;
-export default meta;
-type Story = StoryObj<typeof MultipleSelect>;
-export declare const Primary: Story;
