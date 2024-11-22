15b2c91bd074cf46bcfcf79044c830eb598344e5
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-11-22 17:16:59 +0530

    fonts added

--- lib/src/core-components/atoms/pdf/pdf-typography.components.d.ts
+++ lib/src/core-components/atoms/pdf/pdf-typography.components.d.ts
@@ -11,10 +11,10 @@ interface PdfTextProps {
     fixed?: boolean;
     children?: React.ReactNode;
 }
-export declare const PdfHeading: ({ fontWeight, fontSize, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfSubHeading: ({ fontWeight, fontSize, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfMedium: ({ fontWeight, fontSize, textAlign, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfRegular: ({ fontWeight, fontSize, textAlign, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfSmall: ({ fontSize, fontWeight, textAlign, lineHeight, fixed, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfHeading: ({ fontWeight, fontSize, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfSubHeading: ({ fontWeight, fontSize, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfMedium: ({ fontWeight, fontSize, textAlign, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfRegular: ({ fontWeight, fontSize, textAlign, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfSmall: ({ fontSize, fontFamily, fontWeight, textAlign, lineHeight, fixed, style, children, }: PdfTextProps) => React.JSX.Element;
 export declare const PdfPageNumber: ({ style, bottom }: PdfTextProps) => React.JSX.Element;
 export {};
