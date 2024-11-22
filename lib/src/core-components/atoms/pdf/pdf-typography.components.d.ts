abb908dfb9cd03b8b2ac50eb9c8d1ef804fe25f3
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-11-22 13:27:06 +0530

    font update

--- lib/src/core-components/atoms/pdf/pdf-typography.components.d.ts
+++ lib/src/core-components/atoms/pdf/pdf-typography.components.d.ts
@@ -3,7 +3,7 @@ import { Style } from '@react-pdf/types';
 interface PdfTextProps {
     fontWeight?: 'bold' | 'medium' | 'normal';
     fontSize?: number;
-    fontFamily?: 'ArimaRegular' | string;
+    fontFamily?: 'ArimaRegular' | 'ArimaBold' | string;
     textAlign?: 'center' | 'left' | 'right';
     style?: Style | Style[];
     lineHeight?: number;
@@ -11,10 +11,10 @@ interface PdfTextProps {
     fixed?: boolean;
     children?: React.ReactNode;
 }
-export declare const PdfHeading: ({ fontWeight, fontSize, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfSubHeading: ({ fontWeight, fontSize, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfMedium: ({ fontWeight, fontSize, textAlign, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfRegular: ({ fontWeight, fontSize, textAlign, fontFamily, style, children, }: PdfTextProps) => React.JSX.Element;
-export declare const PdfSmall: ({ fontSize, fontFamily, fontWeight, textAlign, lineHeight, fixed, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfHeading: ({ fontWeight, fontSize, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfSubHeading: ({ fontWeight, fontSize, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfMedium: ({ fontWeight, fontSize, textAlign, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfRegular: ({ fontWeight, fontSize, textAlign, style, children, }: PdfTextProps) => React.JSX.Element;
+export declare const PdfSmall: ({ fontSize, fontWeight, textAlign, lineHeight, fixed, style, children, }: PdfTextProps) => React.JSX.Element;
 export declare const PdfPageNumber: ({ style, bottom }: PdfTextProps) => React.JSX.Element;
 export {};
