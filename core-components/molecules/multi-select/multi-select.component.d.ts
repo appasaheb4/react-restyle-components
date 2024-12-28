ac152eca0d0291f278295cbbc761566c03b4f48a
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-12-28 10:35:44 +0530

    Updates

--- core-components/molecules/multi-select/multi-select.component.d.ts
+++
@@ -1,9 +0,0 @@
-interface MultiSelectProps {
-    options: Array<string>;
-    selectedItems?: Array<string>;
-    hasError?: boolean;
-    onSelect: (item: any) => any;
-}
-export declare const MultiSelect: ({ options, selectedItems, hasError, onSelect, }: MultiSelectProps) => import('../../../../node_modules/react/jsx-runtime').JSX.Element;
-export {};
-//# sourceMappingURL=multi-select.component.d.ts.map
