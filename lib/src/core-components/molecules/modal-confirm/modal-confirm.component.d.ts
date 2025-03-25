84d168f1f38b07c1a21de54681bfedc816f634c9
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-03-25 09:21:32 +0530

    multi selection comp. added

--- lib/src/core-components/molecules/modal-confirm/modal-confirm.component.d.ts
+++ lib/src/core-components/molecules/modal-confirm/modal-confirm.component.d.ts
@@ -1,12 +1,15 @@
+import React from 'react';
 interface ModalConfirmProps {
     visible: boolean;
     title?: string;
-    message: string;
+    message: React.ReactNode | string;
     submitTitle?: string;
     closeTitle?: string;
+    isClick?: boolean;
+    isClose?: boolean;
     onClick: () => void;
     onClose: () => void;
 }
-export declare const ModalConfirm: ({ visible, title, message, submitTitle, closeTitle, onClick, onClose, }: ModalConfirmProps) => import("react/jsx-runtime").JSX.Element;
+export declare const ModalConfirm: ({ visible, title, message, submitTitle, closeTitle, isClick, isClose, onClick, onClose, }: ModalConfirmProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=modal-confirm.component.d.ts.map
