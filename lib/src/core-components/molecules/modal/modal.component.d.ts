69821b20116bf022e1f2de649cc9563422ff046c
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-05-18 15:33:45 +0530

    modal props update

--- lib/src/core-components/molecules/modal/modal.component.d.ts
+++ lib/src/core-components/molecules/modal/modal.component.d.ts
@@ -1,10 +1,10 @@
 import React from 'react';
 export interface ModalProps {
     visible: boolean;
-    title?: string;
-    onClose: () => void;
+    title: string;
+    onClose?: () => void;
     isAutoClose?: boolean;
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }
 export declare const Modal: ({ visible, title, onClose, isAutoClose, children, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=modal.component.d.ts.map
