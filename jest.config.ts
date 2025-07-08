8bcc7ce470119cffd48831f5fd41f8a7e3ad8f7b
appasaheb4 <onlyappasaheb4@gmail.com>
2025-07-08 16:26:00 +0530

    jest update

--- jest.config.ts
+++ jest.config.ts
@@ -1,49 +1,11 @@
-import type { Config } from '@jest/types';
-
-const projects = [
-  {
-    displayName: 'react-restyle-components',
-    // rootDir: '<rootDir>/react-restyle-components',
-    testEnvironment: 'jsdom',
-    // setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-    testPathIgnorePatterns: ['(.*).spec.[jt]sx?'],
-  },
-];
-
-const ignoreNodeModules = [
-  '@hassanmojab/react-modern-calendar-datepicker',
-].join('|');
-
-const config: Config.InitialOptions = {
-  verbose: true,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/**/types.ts',
-    '!<rootDir>/src/**/*.d.ts',
-    '!<rootDir>/src/**/index.{js,jsx,ts,tsx}',
-    '!/src/util/stories/**/*',
-    '!<rootDir>/src/**/__mocks__/**/*',
-    '!<rootDir>/src/**/*.stories.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/**/*.hiddenStories.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/stories/**',
-    '!**/**/illustrationsList.ts',
-    '!<rootDir>/**/iconTypes.ts',
-    '!<rootDir>/**/Templates/StoryComponents/**/*',
-  ],
-  coverageThreshold: {
-    global: {
-      statements: 50,
-      branches: 50,
-      lines: 50,
-      functions: 50,
-    },
-  },
-  projects,
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': require.resolve('babel-jest'),
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  transformIgnorePatterns: [`/node_modules/(?!${ignoreNodeModules})`],
+  moduleNameMapper: {
+     '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.(css|scss|sass|less|style|svg)$': '<rootDir>/__mocks__/styleMock.ts',
+  },
 };
-
-export default config;
