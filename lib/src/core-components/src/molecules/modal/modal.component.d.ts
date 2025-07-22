062194ded0baaaa44fa22cf77b8c5a23286928ae
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-07-22 18:49:11 +0530

    feat: update Modal component to include className and style props

--- lib/src/core-components/src/molecules/modal/modal.component.d.ts
+++ lib/src/core-components/src/molecules/modal/modal.component.d.ts
@@ -2,9 +2,11 @@ import React from 'react';
 export interface ModalProps {
     visible: boolean;
     title: string;
+    className?: string;
+    style?: React.CSSProperties;
     onClose?: () => void;
     isAutoClose?: boolean;
     children?: React.ReactNode;
 }
-export declare const Modal: ({ visible, title, onClose, isAutoClose, children, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Modal: ({ visible, title, className, style, onClose, isAutoClose, children, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=modal.component.d.ts.map
