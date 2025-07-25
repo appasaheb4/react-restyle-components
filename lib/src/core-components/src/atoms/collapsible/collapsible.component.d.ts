4fec169ad848eacf5abb47155f0dfa4f2f054554
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-07-25 12:02:14 +0530

    versin update

--- lib/src/core-components/src/atoms/collapsible/collapsible.component.d.ts
+++ lib/src/core-components/src/atoms/collapsible/collapsible.component.d.ts
@@ -3,8 +3,10 @@ interface CollapsibleProps {
     label: string;
     labelClassName?: string;
     className?: string;
+    onExpand?: (label: string) => void;
+    onCollapse?: (label: string) => void;
     children: React.ReactNode;
 }
-export declare const Collapsible: ({ label, labelClassName, className, children, }: CollapsibleProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Collapsible: ({ label, labelClassName, className, onExpand, onCollapse, children, }: CollapsibleProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=collapsible.component.d.ts.map
