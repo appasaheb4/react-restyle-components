6f7b994f84662e280bcea98465b08e578b329f69
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-09-10 14:49:01 +0530

    storybook update

--- lib/src/core-components/src/molecules/single-select/single-select.component.d.ts
+++ lib/src/core-components/src/molecules/single-select/single-select.component.d.ts
@@ -1,9 +1,12 @@
+import React from 'react';
 interface SingleSelectProps {
     options: Array<any>;
     displayKey?: string;
     hasError?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
     onSelect: (item: any) => any;
 }
-export declare const SingleSelect: ({ options, displayKey, hasError, onSelect, }: SingleSelectProps) => import("react/jsx-runtime").JSX.Element;
+export declare const SingleSelect: ({ options, displayKey, hasError, className, style, onSelect, }: SingleSelectProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=single-select.component.d.ts.map
