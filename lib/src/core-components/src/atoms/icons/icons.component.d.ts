485be802d9244a9f0c576ef2f370b455626c54db
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-09-10 14:06:37 +0530

    version update

--- lib/src/core-components/src/atoms/icons/icons.component.d.ts
+++ lib/src/core-components/src/atoms/icons/icons.component.d.ts
@@ -2,10 +2,11 @@ import { IconBaseProps } from 'react-icons/lib';
 interface IconProps {
     nameIcon: string;
     propsIcon?: IconBaseProps;
+    className?: string;
     tooltip?: string;
     isDisable?: boolean;
     onClick?: () => void;
 }
-export declare const Icon: ({ nameIcon, propsIcon, tooltip, isDisable, onClick, }: IconProps) => JSX.Element;
+export declare const Icon: ({ nameIcon, propsIcon, className, tooltip, isDisable, onClick, }: IconProps) => JSX.Element;
 export {};
 //# sourceMappingURL=icons.component.d.ts.map
---
+++ lib/src/core-components/src/molecules/single-select/single-select.component.d.ts
@@ -0,0 +1,9 @@
+interface SingleSelectProps {
+    options: Array<any>;
+    displayKey?: string;
+    hasError?: boolean;
+    onSelect: (item: any) => any;
+}
+export declare const SingleSelect: ({ options, displayKey, hasError, onSelect, }: SingleSelectProps) => import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=single-select.component.d.ts.map
