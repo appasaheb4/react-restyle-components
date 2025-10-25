a245fea7a5ba5e4e54c08a34513258d85dcb6ddc
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-10-25 17:16:34 +0530

    utils modify

---
+++ lib/src/core-utils/src/utility/utility.util.d.ts
@@ -0,0 +1,4 @@
+export declare const debounce: any;
+export declare const calculateHoursDifference: (date1: Date, date2: Date) => number;
+export declare const calculateHoursDifferenceDecimal: (date1: Date, date2: Date) => number;
+//# sourceMappingURL=utility.util.d.ts.map
