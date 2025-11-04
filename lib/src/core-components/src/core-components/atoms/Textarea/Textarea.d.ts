f2f345fc88a499cc5b0acf2e15e7f0e56c77f9f7
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2025-11-04 10:40:58 +0530

    version update

---
+++ lib/src/core-components/src/core-components/atoms/Textarea/Textarea.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    error?: string;
+    label?: string;
+}
+export declare const Textarea: React.ForwardRefExoticComponent<TextareaProps & React.RefAttributes<HTMLTextAreaElement>>;
+//# sourceMappingURL=Textarea.d.ts.map
